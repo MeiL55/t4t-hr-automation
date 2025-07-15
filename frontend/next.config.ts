d0168d918519c94472236f734b49a9b9cebb4323
fmcafee5 <157412919+fmcafee5@users.noreply.github.com>
2025-07-15 15:07:44 -0700

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  modularizeImports: {
+    'lodash': {
+      transform: 'lodash/{{member}}',
+      preventFullImport: true
+    }
+  },
+  // You can add other Next.js config options here
 };
 
 export default nextConfig;
