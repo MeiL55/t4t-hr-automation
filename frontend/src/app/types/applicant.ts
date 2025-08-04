e16390da620b1fdbb3253882e2f9e9d90d46e75f
Finlay McAfee <157412919+fmcafee5@users.noreply.github.com>
2025-08-04 16:32:57 -0700

    Update applicant.ts

--- frontend/src/app/types/applicant.ts
+++ frontend/src/app/types/applicant.ts
@@ -1,9 +1,11 @@
-export interface Applicant {
-  application_id: number
-  user_id: number
-  name: string
-  email: string
-  telephone: string
-  stage: string
-  resume_url: string
+export type Applicant = {
+  application_id: number;
+  user_id: number;
+  name: string;
+  email: string;
+  telephone: string;
+  stage: string;
+  resume_url: string;
+  interview_stage?: 'interview_1' | 'interview_2'; 
+  interview_notes?: string; 
 }
