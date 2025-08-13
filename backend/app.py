d53b570b25e6c0401b7c3e7e14c1b4ac34172296
MeiL55 <mli783@gatech.edu>
2025-08-13 15:54:27 -0400

    fix import paths

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,10 @@ from flask import Flask
 import sys
 import os
 #sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-from routes.auth import auth_bp
-from routes.apply import user_bp
-from routes.resume import resume_bp
-from routes.hr import hr_dashboard_bp
+from backend.routes.auth import auth_bp
+from backend.routes.apply import user_bp
+from backend.routes.resume import resume_bp
+from backend.routes.hr import hr_dashboard_bp
 from flask_cors import CORS
 
 app = Flask(__name__)
