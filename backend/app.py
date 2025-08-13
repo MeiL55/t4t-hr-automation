5b040c95c0bc1788d02838d3a22e167c7020c833
MeiL55 <mli783@gatech.edu>
2025-08-13 14:43:17 -0400

    change app.py for dockerfile PORT issue

--- backend/app.py
+++ backend/app.py
@@ -16,5 +16,4 @@ app.register_blueprint(resume_bp)
 app.register_blueprint(hr_dashboard_bp)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080, debug=False)
