b705c3819472ead85fb834382c76561570c50ee4
MeiL55 <mli783@gatech.edu>
2025-08-14 22:34:12 -0400

    enable db ipv4

--- backend/models/database.py
+++ backend/models/database.py
@@ -7,7 +7,17 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 DATABASE_URL = os.getenv("SUPABASE_DB_URL")
+#print(DATABASE_URL)
 
-engine = create_engine(DATABASE_URL)
+if "sslmode=" not in (DATABASE_URL or ""):
+    DATABASE_URL += ("&" if "?" in DATABASE_URL else "?") + "sslmode=require"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"hostaddr": os.getenv("PGHOSTADDR")},  # <-- force IPv4
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
